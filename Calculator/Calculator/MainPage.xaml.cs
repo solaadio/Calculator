23bac47b5435f46be5f8997d61a9f44d4ea23993
solaadio <olusola.adio@logion.co.uk>
2019-02-16 18:16:03 +0000

    mainpage changes

--- Calculator/Calculator/MainPage.xaml.cs
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Calculator
@@ -10,9 +6,9 @@ namespace Calculator
     public partial class MainPage : ContentPage
     {
 
-        int currentState = 1;
-        string mathOperator;
-        double firstNumber, secondNumber;
+        int _currentState = 1;
+        string _mathOperator;
+        double _firstNumber, _secondNumber;
 
         public MainPage()
         {
@@ -28,55 +24,55 @@ namespace Calculator
             Button button = (Button)sender;
             string pressed = button.Text;
 
-            if (this.resultText.Text == "0" || currentState < 0)
+            if (this.ResultText.Text == "0" || _currentState < 0)
             {
-                this.resultText.Text = "";
-                if (currentState < 0)
-                    currentState *= -1;
+                this.ResultText.Text = "";
+                if (_currentState < 0)
+                    _currentState *= -1;
             }
 
-            this.resultText.Text += pressed;
+            this.ResultText.Text += pressed;
 
             double number;
-            if (double.TryParse(this.resultText.Text, out number))
+            if (double.TryParse(this.ResultText.Text, out number))
             {
-                this.resultText.Text = number.ToString("N0");
-                if (currentState == 1)
+                this.ResultText.Text = number.ToString("N0");
+                if (_currentState == 1)
                 {
-                    firstNumber = number;
+                    _firstNumber = number;
                 }
                 else
                 {
-                    secondNumber = number;
+                    _secondNumber = number;
                 }
             }
         }
 
         void OnSelectOperator(object sender, EventArgs e)
         {
-            currentState = -2;
+            _currentState = -2;
             Button button = (Button)sender;
             string pressed = button.Text;
-            mathOperator = pressed;
+            _mathOperator = pressed;
         }
 
         void OnClear(object sender, EventArgs e)
         {
-            firstNumber = 0;
-            secondNumber = 0;
-            currentState = 1;
-            this.resultText.Text = "0";
+            _firstNumber = 0;
+            _secondNumber = 0;
+            _currentState = 1;
+            this.ResultText.Text = "0";
         }
 
         void OnCalculate(object sender, EventArgs e)
         {
-            if (currentState == 2)
+            if (_currentState == 2)
             {
                 var result = 0; // SimpleCalculator.Calculate(firstNumber, secondNumber, mathOperator);
 
-                this.resultText.Text = result.ToString("N5");
-                firstNumber = result;
-                currentState = -1;
+                this.ResultText.Text = result.ToString("N5");
+                _firstNumber = result;
+                _currentState = -1;
             }
         }
     }
