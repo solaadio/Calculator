24dd147a16fd076550583033726aeab669f1c2db
Olusola Adio <olusola.adio@logion.co.uk>
2019-02-17 15:36:21 +0000

    Refactored the XAML code

--- Calculator/Calculator/Libraries/SharedResources.cs
+++ Calculator/Calculator/Libraries/SharedResources.cs
@@ -8,5 +8,35 @@ namespace Calculator.Libraries
         {
             get { return Color.FromRgb(0xff, 0xa5, 0); }
         }
+
+        public static String AndroidButtonFontSize
+        {
+            get { return "42"; }
+        }
+
+        public static String IOSButtonFontSize
+        {
+            get { return "42"; }
+        }
+
+        public static String UWPButtonFontSize
+        {
+            get { return "42"; }
+        }
+
+        public static String AndroidLabelFontSize
+        {
+            get { return "64"; }
+        }
+
+        public static String IOSLabelFontSize
+        {
+            get { return "64"; }
+        }
+
+        public static String UWPLabelFontSize
+        {
+            get { return "64"; }
+        }
     }
 }
