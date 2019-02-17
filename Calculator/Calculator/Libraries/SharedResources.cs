17a7f3dae6483bc1e3f8df7b9a0f4bd3daddb93f
Olusola Adio <olusola.adio@logion.co.uk>
2019-02-17 18:02:12 +0000

    chaanged String to string

--- Calculator/Calculator/Libraries/SharedResources.cs
+++ Calculator/Calculator/Libraries/SharedResources.cs
@@ -6,16 +6,16 @@ namespace Calculator.Libraries
     {
         public static Color OpButtonBkColor => Color.FromRgb(0xff, 0xa5, 0);
 
-        public static String AndroidButtonFontSize => "42";
+        public static string AndroidButtonFontSize => "42";
 
-        public static String IosButtonFontSize => "42";
+        public static string IosButtonFontSize => "42";
 
-        public static String UwpButtonFontSize => "42";
+        public static string UwpButtonFontSize => "42";
 
-        public static String AndroidLabelFontSize => "64";
+        public static string AndroidLabelFontSize => "64";
 
-        public static String IosLabelFontSize => "64";
+        public static string IosLabelFontSize => "64";
 
-        public static String UwpLabelFontSize => "64";
+        public static string UwpLabelFontSize => "64";
     }
 }
