9d61898b46ca7f5c0592423eed4cd1c1279c275a
solaadioVirtualBox <olusola.adio@logion.co.uk>
2019-03-27 18:50:26 +0000

    Added a DotNetCore unit test project

---
+++ Calculator.Tests.Nunit/DecimalTests/CalculatorServiceDecimalTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Calculator.Services.Implementations;
+using Calculator.Services.Interfaces;
+using NUnit.Framework;
+
+namespace Calculator.Tests.Nunit.DecimalTests
+{
+    [TestFixture]
+    // ReSharper disable once InconsistentNaming
+    public class CalculatorServiceDecimalTests
+    {
+
+        [Test]
+        public void Test_01_That_One_Half_Plus_One_Half_Equals_Three()
+        {
+            // Arrange
+            const decimal expected = 3;
+            const decimal firstNumber = 1.5M;
+            const decimal secondNumber = 1.5M;
+            const string mathOperator = "+";
+            ICalculatorService service = new CalculatorService();
+
+            // Act
+            var actual = service.CalculateDecimal(firstNumber, secondNumber, mathOperator);
+
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_02_That_One_Half_Minus_One_Quarter_Equals_One_Quarter()
+        {
+            // Arrange
+            decimal expected = .25M;
+            const decimal firstNumber = 0.5M;
+            const decimal secondNumber = .25M;
+            const string mathOperator = "-";
+            ICalculatorService service = new CalculatorService();
+
+            // Act
+            var actual = service.CalculateDecimal(firstNumber, secondNumber, mathOperator);
+
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_03_That_One_Half_Times_One_Half_Equals_One_Quarter()
+        {
+            // Arrange
+            const decimal expected = .25M;
+            const decimal firstNumber = .5M;
+            const decimal secondNumber = .5M;
+            const string mathOperator = "X";
+            ICalculatorService service = new CalculatorService();
+
+            // Act
+            var actual = service.CalculateDecimal(firstNumber, secondNumber, mathOperator);
+
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_04_That_Three_Divided_By_Two_Equals_One_Half()
+        {
+            // Arrange
+            const decimal firstNumber = 3M;
+            const decimal secondNumber = 2M;
+            const string mathOperator = "/";
+            ICalculatorService service = new CalculatorService();
+
+            // Act
+            //Assert
+            Assert.Throws<ArgumentException>(() => service.CalculateDecimal(firstNumber, secondNumber, mathOperator));
+
+        }
+
+        [Test]
+        public void Test_05_That_Four_And_Half_Divided_By_Zero_Does_Not_Crash()
+        {
+            // Arrange
+            const decimal firstNumber = 4.5M;
+            const decimal secondNumber = 0M;
+            const string mathOperator = "/";
+            ICalculatorService service = new CalculatorService();
+
+            // Act
+            //Assert
+            Assert.Throws<ArgumentException>(() => service.CalculateDecimal(firstNumber, secondNumber, mathOperator));
+        }
+
+        [Test]       
+        public void Test_06_That_Minus_Four_And_Half_Divided_By_Zero_Does_Not_Crash()
+        {
+            // Arrange
+            const decimal firstNumber = -4.5M;
+            const decimal secondNumber = 0M;
+            const string mathOperator = "/";
+            ICalculatorService service = new CalculatorService();
+
+            // Act
+            //Assert
+            Assert.Throws<ArgumentException>(() => service.CalculateDecimal(firstNumber, secondNumber, mathOperator));
+
+        }
+
+        [Test]
+        public void Test_07_That_Three_Divided_By_Two_Equals_One_Half()
+        {
+            // Arrange
+            const decimal expected = 1.5M;
+            const decimal firstNumber = 3M;
+            const decimal secondNumber = 2M;
+            const string mathOperator = "÷";
+            ICalculatorService service = new CalculatorService();
+
+            // Act
+            var actual = service.CalculateDecimal(firstNumber, secondNumber, mathOperator);
+
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_08_That_Four_And_Half_Divided_By_Zero_Does_Not_Crash()
+        {
+            // Arrange
+            const decimal firstNumber = 4.5M;
+            const decimal secondNumber = 0M;
+            const string mathOperator = "÷";
+            ICalculatorService service = new CalculatorService();
+
+            // Act
+            //Assert
+            Assert.Throws<DivideByZeroException>(() => service.CalculateDecimal(firstNumber, secondNumber, mathOperator));
+        }
+
+        [Test]
+        public void Test_09_That_Minus_Four_And_Half_Divided_By_Zero_Does_Not_Crash()
+        {
+            // Arrange
+            const decimal firstNumber = -4.5M;
+            const decimal secondNumber = 0M;
+            const string mathOperator = "÷";
+            ICalculatorService service = new CalculatorService();
+
+            // Act
+            //Assert
+            Assert.Throws<DivideByZeroException>(() => service.CalculateDecimal(firstNumber, secondNumber, mathOperator));
+
+        }
+
+
+
+    }
+
+}
